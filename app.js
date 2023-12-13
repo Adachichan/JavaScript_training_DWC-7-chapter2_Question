57603bc4638425ef296babf7eabe48d08f0ac18e
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-13 13:51:57 +0000

    [add] #2 Chapter2_app.js "四則演算(第2章の確認問題)"

---
+++ app.js
@@ -0,0 +1,19 @@
+let num1 = 6;
+let num2 = 4;
+let num3 = 2;
+
+// 足し算
+let num4 = num1 + num2 + num3;
+alert(num1 + '+' + num2 + '+' + num3 + '=' + num4 + 'です');
+
+// 引き算
+let num5 = num1 - num2 - num3;
+alert(num1 + '-' + num2 + '-' + num3 + '=' + num5 + 'です');
+
+// 掛け算
+let num6 = num1 * num2 * num3;
+alert(num1 + '×' + num2 + '×' + num3 + '=' + num6 + 'です');
+
+// 割り算
+let num7 = num1 / num3;
+alert(num1 + '÷' + num3 + '=' + num7 + 'です');
